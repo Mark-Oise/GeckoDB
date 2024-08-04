9bf54e3fee1eab542fc0843ebb0ad2754ad384cb
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2024-08-04 14:06:21 +0100

    Add core database operations to GeckoDBServer
    - Implement handle_get, handle_set, handle_delete methods
    - Add handle_flush for clearing all data
    - Implement handle_mget and handle_mset for bulk operations
    - Integrate GeckoDB data types for value storage

--- geckodb/server.py
+++ geckodb/server.py
@@ -1,5 +1,5 @@
 import gevent
-
+from .data_types import GeckoDBString, GeckoDBNumber, GeckoDBNull, GeckoDBArray, GeckoDBDict
 
 class GeckoDBServer:
     """
@@ -66,3 +66,90 @@ class GeckoDBServer:
             return self.handle_mset(command['pairs'])
         else:
             raise ValueError(f'Unknown command: {cmd}')
+
+    def handle_get(self, key):
+        """
+        Retrieve the value associated with the given key.
+
+        Args:
+            key: The key to look up in the database.
+
+        Returns:
+            The value associated with the key, or None if the key doesn't exist.
+        """
+        return self.data.get(key, GeckoDBNull()).value
+
+    def handle_set(self, key, value):
+        """
+        Set a key-value pair in the database.
+
+        Args:
+            key: The key to set.
+            value: The value to associate with the key.
+
+        Returns:
+            'OK' to indicate successful operation.
+        """
+        # Determine the type of value and create appropriate GeckoDB object
+        if isinstance(value, (int, float)):
+            self.data[key] = GeckoDBNumber(value)
+        elif isinstance(value, list):
+            self.data[key] = GeckoDBArray(value)
+        elif isinstance(value, dict):
+            self.data[key] = GeckoDBDict(value)
+        elif value is None:
+            self.data[key] = GeckoDBNull()
+        else:
+            self.data[key] = GeckoDBString(value)
+        return 'OK'
+
+    def handle_delete(self, key):
+        """
+        Delete a key-value pair from the database.
+
+        Args:
+            key: The key to delete.
+
+        Returns:
+            'OK' if the key was deleted, 'NULL' if the key didn't exist.
+        """
+        if key in self.data:
+            del self.data[key]
+            return 'OK'
+        return 'NULL'
+
+    def handle_flush(self):
+        """
+        Clear all data from the database.
+
+        Returns:
+            'OK' to indicate successful operation.
+        """
+        self.data.clear()
+        return 'OK'
+
+    def handle_mget(self, keys):
+        """
+        Retrieve multiple values associated with the given keys.
+
+        Args:
+            keys: A list of keys to look up in the database.
+
+        Returns:
+            A list of values corresponding to the given keys.
+        """
+        return [self.handle_get(key) for key in keys]
+
+    def handle_mset(self, pairs):
+        """
+        Set multiple key-value pairs in the database.
+
+        Args:
+            pairs: A dictionary of key-value pairs to set.
+
+        Returns:
+            'OK' to indicate successful operation.
+        """
+        for key, value in pairs.items():
+            self.handle_set(key, value)
+        return 'OK'
